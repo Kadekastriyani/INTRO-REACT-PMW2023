da83bef74d36f74e206d41dd730d12c214287e67
Kadek Astri Yani <kadekastriyani@gmail.com>
2023-11-02 15:20:29 +0800

    update button

--- src/app/page.js
+++ src/app/page.js
@@ -2,22 +2,50 @@ export default function Home() {
   return (
     <section
       style={{
+        width: "100vw",
+        height: "100vh",
         display: "flex",
         flexDirection: "column",
-        alignItems: "center",  // Mengatur elemen secara horizontal ke tengah
-        justifyContent: "center", // Mengatur elemen secara vertikal ke tengah
-        height: "100vh", // Menentukan tinggi elemen sebagai tinggi tampilan
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
       <h1
         style={{
           margin: 0,
-          marginBottom: "20px",
+          marginBottom: "12px",
+          fontSize: "24px",
         }}
       >
         Welcome to my App
       </h1>
-      <button style={{ width: "200px", height: "50px" }}>Hallo</button>
+      <HelloButton buttonText={"button1"} buttonText2={"button2"} />
     </section>
+  );
+}
+
+function HelloButton({ buttonText, buttonText2 }) {
+  return (
+    <>
+      <button
+        style={{
+          width: "240px",
+          height: "36px",
+        }}
+      >
+        {" "}
+        {buttonText}{" "}
+      </button>
+      <button
+        style={{
+          width: "240px",
+          height: "36px",
+          marginTop: "12px",
+        }}
+      >
+        {" "}
+        {buttonText2}{" "}
+      </button>
+    </>
   );
 }
